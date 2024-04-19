b5074c9fc7017099b5128d541013ef7b362fc798
Claire Perroux <perroux.cl@gmail.com>
2024-04-19 11:50:43 +0200

    ajout incon

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import './index.css';
 import { Clock } from './Clock/Clock';
-import { Vector2D } from './Math/Vector2D';
 import { Matrix3x3 } from './Math/Matrix3x3';
 
 window.onload = function() {
     document.getElementById('addClockButton').addEventListener('click', () => {
         const hourOffsetInput = document.getElementById('hourOffsetInput') as HTMLInputElement;
         const hourOffset = parseInt(hourOffsetInput.value);
-        new Clock(hourOffset);
+        if (isNaN(hourOffset)) {
+            alert('Veuillez entrer un nombre valide pour le décalage horaire.');
+        } else {
+            new Clock(hourOffset);
+        }
     });
-
 };
 
 
